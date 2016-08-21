4ce6dca4a10e6d64832b2671a66d9bb301c422d8
radicis <alloyddesign@gmail.com>
2016-08-21 11:36:22 +0100

    reverted structure changes. Added a wrestler type field to sort by. Reduces code duplication in service and allows previous exists/delete methods to still work

--- www/js/controllers/wrestlers.js
+++ www/js/controllers/wrestlers.js
@@ -19,88 +19,93 @@ angular.module('starter').controller('WrestlersCtrl', function($scope, Wrestlers
     $scope.hide();
   });
 
-
-
-// displays a confirmation to the user and dletes the wrestler is confirmed
-$scope.delete = function(wrestler){
-  $scope.check = {};
-  var myPopup = $ionicPopup.show({
-    templateUrl: 'templates/modals/confirmDelete.html',
-    title: 'Enter Name',
-    scope: $scope,
-    buttons: [
-      { text: 'Cancel' },
-      {
-        text: '<b>Delete</b>',
-        type: 'button-assertive',
-        onTap: function(e) {
-          if ($scope.check.confirmDelete==true) {
-            Wrestlers.delete(wrestler);
-          } else {
-            myPopup.close();
+  // displays a confirmation to the user and dletes the wrestler is confirmed
+  $scope.delete = function(wrestler){
+    $scope.check = {};
+    var myPopup = $ionicPopup.show({
+      templateUrl: 'templates/modals/confirmDelete.html',
+      title: 'Enter Name',
+      scope: $scope,
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: '<b>Delete</b>',
+          type: 'button-assertive',
+          onTap: function(e) {
+            if ($scope.check.confirmDelete==true) {
+              Wrestlers.delete(wrestler);
+            } else {
+              myPopup.close();
+            }
           }
         }
-      }
-    ]
-  });
-}
+      ]
+    });
+  };
 
 // Adds a new wrestler to the firebase and tries to pull the image from wiki
-$scope.addWrestler = function(){
-  $scope.newWrestler = {};
+  $scope.addWrestler = function(){
+    $scope.newWrestler = {};
 
-  var myPopup = $ionicPopup.show({
-    templateUrl: 'templates/modals/addWrestler.html',
-    title: 'Enter Name',
-    scope: $scope,
-    buttons: [
-      { text: 'Cancel' },
-      {
-        text: '<b>Save</b>',
-        type: 'button-balanced',
-        onTap: function(e) {
-          if (!$scope.newWrestler.name) {
-            e.preventDefault();
-          } else {
-            $scope.saveWrestler($scope.newWrestler);
-            myPopup.close();
+    var myPopup = $ionicPopup.show({
+      templateUrl: 'templates/modals/addWrestler.html',
+      title: 'Enter Name',
+      scope: $scope,
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: '<b>Save</b>',
+          type: 'button-balanced',
+          onTap: function(e) {
+            if (!$scope.newWrestler.name) {
+              e.preventDefault();
+            } else {
+              $scope.saveWrestler($scope.newWrestler);
+              myPopup.close();
+            }
           }
         }
-      }
-    ]
-  });
-};
+      ]
+    });
+  };
 
-$scope.saveWrestler = function(wrestler) {
-  /*Wrestlers.exists(wrestler.name).then(function(exists){
-    console.log(exists);
-    if(exists != false){
-      alert("Already exists in the database!");
-    }
-    else{*/
-      console.log(wrestler);
-      var wrass = Wrestlers.getWikiInfo(wrestler.name);
-      var img;
-      wrass.then(function(response){
-        try{
-          var pageId = response.data.continue.imcontinue.split("|")[0];
-          var pages = response.data.query.pages;
-          var imgsrc = pages[pageId];
-          img = imgsrc.thumbnail.source;
-        }catch(err){img = null}
-        var wrestlerToAdd = {
-          name:  wrestler.name.split('(')[0],
-          active: true,
-          pic: img
-        };
-        if(wrestler.isFemale){
-          Wrestlers.addFemale(wrestlerToAdd);
-        } else if (wrestler.isTeam){
-          Wrestlers.addTag(wrestlerToAdd);
-        } else {
-          Wrestlers.addMale(wrestlerToAdd);
-        }
-        console.log('Created!', wrestler.name);
-      });
+  $scope.saveWrestler = function(wrestler) {
+    Wrestlers.exists(wrestler.name).then(function(exists) {
+      console.log(exists);
+      if (exists != false) {
+        alert("Already exists in the database!");
+      }
+      else {
+        console.log(wrestler);
+        var wrass = Wrestlers.getWikiInfo(wrestler.name);
+        var img;
+        wrass.then(function (response) {
+          try {
+            var pageId = response.data.continue.imcontinue.split("|")[0];
+            var pages = response.data.query.pages;
+            var imgsrc = pages[pageId];
+            img = imgsrc.thumbnail.source;
+          } catch (err) {
+            img = null
+          }
+          var type = 0;
+          if (wrestler.isFemale) {
+            type = 1
+          } else if (wrestler.isTeam) {
+            type = 2;
+          }
+
+          var wrestlerToAdd = {
+            name: wrestler.name.split('(')[0],
+            active: true,
+            pic: img,
+            type: type
+          };
+
+          Wrestlers.add(wrestlerToAdd);
+          console.log('Created!', wrestler.name);
+        });
+      }
+    });
   };
 });
--- www/js/services/wrestlers.js
+++ www/js/services/wrestlers.js
@@ -1,21 +1,20 @@
 angular.module('starter').service('Wrestlers', function($q, $http, $firebaseArray, $firebaseObject) {
 
   var wrestlerRef = firebase.database().ref().child("wrestlers");
-  var maleRef = wrestlerRef.child("male");
-  var tagRef = wrestlerRef.child("tag");
-  var femaleRef = wrestlerRef.child("female");
+  var maleRef = wrestlerRef.orderByChild("type").equalTo(0);
+  var tagRef = wrestlerRef.orderByChild("type").equalTo(2);
+  var femaleRef = wrestlerRef.orderByChild("type").equalTo(1);
 
   this.getAll = function(){
     return $firebaseArray(wresterlerRef);
   };
-
 
   this.getAllMale = function(){
     var def = $q.defer();
     var maleWrestlers =  $firebaseArray(maleRef);
     maleWrestlers.$loaded().then(function(snap){
       def.resolve(snap);
-    })
+    });
     return def.promise;
   };
 
@@ -24,7 +23,7 @@ angular.module('starter').service('Wrestlers', function($q, $http, $firebaseArra
     var tagWrestlers =  $firebaseArray(tagRef);
     tagWrestlers.$loaded().then(function(snap){
       def.resolve(snap);
-    })
+    });
     return def.promise;
   };
 
@@ -33,44 +32,36 @@ angular.module('starter').service('Wrestlers', function($q, $http, $firebaseArra
     var femaleWrestlers =  $firebaseArray(femaleRef);
     femaleWrestlers.$loaded().then(function(snap){
       def.resolve(snap);
-    })
+    });
     return def.promise;
   };
 
-  this.addMale = function(wrestler){
-    maleRef.push(wrestler);
-  };
-
-  this.addFemale = function(wrestler){
-    femaleRef.push(wrestler);
-  };
-
-  this.addTag = function(wrestler){
-    tagRef.push(wrestler);
+  this.add = function(wrestler){
+    wrestlerRef.push(wrestler);
   };
 
   this.exists = function(name){
     var def = $q.defer();
-    var thisWrestler = $firebaseArray(wresterlerRef.orderByChild("name").equalTo(name));
+    var thisWrestler = $firebaseArray(wrestlerRef.orderByChild("name").equalTo(name));
     thisWrestler.$loaded().then(function(snap){
       try{
         def.resolve(snap[0].$id);
       }catch(err){def.resolve(false);}
     });
     return def.promise;
-  }
+  };
 
   this.get = function(id){
     var def = $q.defer();
-    var wrestler = $firebaseArray(wresterlerRef);
+    var wrestler = $firebaseArray(wrestlerRef);
     wrestler.$loaded().then(function(snap){
       def.resolve(snap.$getRecord(id));
-    })
+    });
     return def.promise;
-  }
+  };
 
   this.delete = function(wrestler){
-    wresterlerRef.child(wrestler.$id).remove();
+    wrestlerRef.child(wrestler.$id).remove();
   };
 
   this.getWikiInfo = function(name){
